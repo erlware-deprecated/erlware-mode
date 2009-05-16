141d6a2b7551a15361ebab3bd4cdca9df03a4835
Dave Peticolas <dave@krondo.com>
2009-05-15 18:58:06 -0700

    Accomodate erlware erlc wrapper.

--- flymaker.sh
+++ flymaker.sh
@@ -5,10 +5,10 @@ echo $1
 Is=""
 PAs=""
 for i in `dirname $1`/../../*/include; do
-    Is="-I$i $Is"
+    Is="-I $i $Is"
 done
 for e in `dirname $1`/../../*/ebin; do
-    PAs="-pa$e $PAs"
+    PAs="-pa $e $PAs"
 done
 
-erlc -o/tmp $Is $PAs -Wall -P $1
+erlc -o /tmp $Is $PAs -Wall $1
